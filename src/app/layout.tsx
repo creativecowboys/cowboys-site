7692c98da5e65ce459120d2841e1b8fb3479dafb
Creative Cowboys <josh@creativecowboys.co>
2026-04-21 13:25:22 -0400

    feat: install Meta Pixel (ID: 1804089387217105)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -153,6 +153,25 @@ export default function RootLayout({
           gtag('config', 'G-CSND3EQCNS');
         `}
       </Script>
+      {/* Meta Pixel */}
+      <Script id="meta-pixel" strategy="afterInteractive">
+        {`
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '1804089387217105');
+          fbq('track', 'PageView');
+        `}
+      </Script>
+      <noscript>
+        {`<img height="1" width="1" style="display:none"
+          src="https://www.facebook.com/tr?id=1804089387217105&ev=PageView&noscript=1"/>`}
+      </noscript>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#0D0D0F]`}
       >
